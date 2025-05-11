19af1362bba79a638822ce41056dc6ee9240714c
memem47 <nrg34423@nifty.com>
2025-05-11 21:03:41 +0900

    feat: draw 10x20 grid

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import sys
 
 # 初期化
 pygame.init()
-WIDTH, HEIGHT = 300, 600
+GRID_SIZE = 30
+COLS, ROWS = 10, 20
+WIDTH, HEIGHT = COLS * GRID_SIZE, ROWS * GRID_SIZE
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Python Tetris")
 
@@ -17,8 +19,14 @@ while True:
             sys.exit()
 
     # 画面を黒で塗りつぶす
-    screen.fill((0, 0, 0))
+    screen.fill("black")
 
+    # draw grid
+    for x in range(0, WIDTH, GRID_SIZE):
+        pygame.draw.line(screen, "dimgray", (x, 0), (x, HEIGHT))
+    for y in range(0, HEIGHT, GRID_SIZE):
+        pygame.draw.line(screen, "dimgray", (0, y), (WIDTH, y))
+        
     # 画面を更新
     pygame.display.flip()
 
